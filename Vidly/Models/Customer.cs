03ca6bc2ff11d7206c212cd47698575a8a901bf9
yashgoswami <yashgos@gmail.com>
2017-07-22 12:22:36 +0530

    Adding migrations to Vidly

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,5 +9,14 @@ namespace Vidly.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        /// <summary>
+        /// This is called Navigation property
+        /// </summary>
+        public MembershipType MembershipType { get; set; }
+
+        // EF will treat this as a foreign key
+        public byte MembershipTypeId { get; set; }
     }
 }
---
+++ Vidly/Models/MembershipType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class MembershipType
+    {
+        // In EF every entity must have a key
+        // by naming convention either Id or className+Id
+        public byte Id { get; set; }
+
+        public short SignUpFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
