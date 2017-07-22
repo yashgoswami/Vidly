3c521f58addd9712190b7c024d82a2800ef7306e
yashgoswami <yashgos@gmail.com>
2017-07-22 12:58:17 +0530

    Updated Controller to fetch data from database put it in viewModel

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -10,20 +10,46 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose(); 
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = new List<Customer>();
-            customers.Add(new Customer { ID = 1, Name = "Yash" });
-            customers.Add(new Customer { ID = 2, Name = "Paras" });
+            var customerList = _context.Customers.ToList();
+            //var customers = new List<Customer>();
+            //customers.Add(new Customer { ID = 1, Name = "Yash" });
+            //customers.Add(new Customer { ID = 2, Name = "Paras" });
 
-            var customerList = new Customers()
+            //var customerList = new Customers()
+            //{
+            //    CustomersList = customers
+            //};
+
+
+            var CustomersViewModel = new Customers
             {
-                CustomersList = customers
+                CustomersList = customerList
             };
 
+            return View(CustomersViewModel);
+        }
 
-            return View(customerList);
+        public ActionResult Detail(int id)
+        {
+            var customer = _context.Customers.Where(m => m.ID == id);
+            if (customer == null)
+                return HttpNotFound();
+            return Content("Customer Name: "+ id);
         }
     }
 }
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,10 @@ namespace Vidly.Models
     public class Customer
     {
         public int ID { get; set; }
+
+        // Adding attributes to decide db field proerties
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
 
